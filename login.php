75a2c1acae1c7db7b3cf99b5130fb57ad9923518
RodrigoNC <rodrigolnr@gmail.com>
2015-06-03 03:03:14 -0300

    login.php removido <?php ?> desnecessario

--- login.php
+++ login.php
@@ -75,14 +75,7 @@
             include_once(TEMPLATES.'/geral/footer.php');
         ?>
             </div>
-        <?php
-            //include_once(TEMPLATES.'/oxe/preview.php');
-        ?>
         </div>
-
-
-
-    	
     </body>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
