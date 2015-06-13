696978c9597e37346a55f497666456b5c625fff9
RodrigoNC <rodrigolnr@gmail.com>
2015-06-13 03:03:55 -0300

    página home indisponível

--- templates/oxe/home.php
+++ templates/oxe/home.php
@@ -1,3 +1,6 @@
-<?php  
-	echo "home OXE!";
-?>
+<div align='center' class='centered-20'>
+	<div class='header-2'>
+		<h1 style='color:#005C83;'><b>Página indisponível no momento</b></h1>
+	</div>
+	<img src="/oxe/estaticos/source/imgs/thumbs.png">
+</div>
